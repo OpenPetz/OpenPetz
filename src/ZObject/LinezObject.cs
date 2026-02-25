c4d60b98059f607c6733f19a4b890e5d51f54643
Ziv <97392652+zivragon@users.noreply.github.com>
2026-02-25 12:51:23 +0330

    Add files via upload

--- src/ZObject/LinezObject.cs
+++ src/ZObject/LinezObject.cs
@@ -6,14 +6,18 @@ using OpenPetz;
 
 public partial class LinezObject : Node2D
 {
-	public OpenPetz.Linez.Database Linez = new OpenPetz.Linez.Database();
+	public OpenPetz.Linez.Database LinezDatabase = new OpenPetz.Linez.Database();
 	
 	public List<Fudger> Fudgers = new List<Fudger>();
 	
-	public Vector3 Rotation3D = new Vector3(-0.125f, 0.0f, 0.0f);
+	public Vector3 Rotation3D = new Vector3(0.125f, 0.0f, 0.0f);
+	
+	public BallzModel catBhd;
 	
 	public LinezObject()
 	{
+		LinezDatabase.Parse("");
 		
+		catBhd = AnimationManager.FetchCatBhd();
 	}
 }
